a7d3e1889bbec01a27bcdb9206c099d2fda8a50b
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-02-04 13:37:15 +0900

    Delete data directory

--- data/talk.py
+++
@@ -1,6 +0,0 @@
-talkdata = {
-    "1":{
-        "ja":"こんにちは。こんな時に初めて見る人なんか珍しいね。\n名前?僕はただのなんてこともない老人だよ。\n君の名前は?{ctx.author.name}さんか。よろしく。\nところでここはどこか分かる?え?わからないって?\n君は実に面白いなー。ここはセーフイ村だよ。\n地上は危ないわけだから、みんなここで暮らしてるわけだよ。\nえっ?出たい?(なんでここを出たい人はみんな僕に言うんだろう...)\n本当に出たいなら一つすることがある。\nあそこの店に行って、を買ってきて。それができれば出してあげるよ。\n```diff\n! ミッションクリア !\n```",
-        "en":"Hi there! Say, you're new here aren't you? That's very rare in these times.\nWho am I? Don't worry, that isn't important. I'm just an old man who lives here.\nBy the way, who are you? Oh, you're {ctx.author.name}? nice to meet you!\nWelcome to Safiy Village, the safest village/bunker/tourist attraction around here!\nAlthough, you certainly don't seem like the person who would like to stay put...\nOkay, tell you what. If you can help me,\nI'll help you get out of here. Agreed? Nice!\nNow firstly, I'm going to have to ask you to get a stick. Not going to explain, just go get it. You'll understand why."
-    }
-}
