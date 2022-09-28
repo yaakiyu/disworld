7ce6af39548e73ea52d4bbc2fb82dc2060788a38
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-09-28 01:40:44 +0000

    update: shop2回目以降を復旧

--- cogs/shop.py
+++ cogs/shop.py
@@ -13,74 +13,86 @@ class Shop(commands.Cog):
         self.bot = bot
 
     @commands.hybrid_command(name="shop")
-    async def c_shop(self, ctx):
+    async def c_shop(self, ctx: commands.Context):
         await self.bot.lock_checker(ctx, 3, 0.2)
 
         udata = self.bot.db.user[ctx.author.id]["Story"]
         if udata == 3:
             # チュートリアル
             return await self.tutorial(ctx)
+
+        udata = self.bot.db.user[ctx.author.id]
+        uitem = orjson.loads(self.bot.db.item[ctx.author.id]["Data"])
+        places = [p for p in self.bot.fielddata if p["visit"] < udata[2]]
+        shops = [x for s in places for x in s["shop"]]
+        view = utils.EasyView(utils.EasyMenu(
+            "お店を選択", 
+            "行きたいお店を選択してください。",
+            **{s["name"]:str(n) for n, s in enumerate(shops)}
+        ))
+        msg = await ctx.send(
+            embed=discord.Embed(
+                title="ショップ",
+                description="行きたいお店に行くことができます。"
+            ),
+            view=view
+        )
+        def inter_check(inter: discord.Interaction):
+            return (inter.user == ctx.author
+                and inter.type == discord.InteractionType.component
+                and inter.channel_id == ctx.channel.id
+                and inter.message is not None and inter.message.id == msg.id
+            )
+        inter: discord.Interaction = await self.bot.wait_for(
+            "interaction", check=inter_check
+        )
+        select: discord.ui.Select = view.children[0]  # type: ignore
+        selected = shops[int(select.values[0])]
+        selling_items = [self.bot.itemdata[l] for l in selected["items"]]
+        embed = discord.Embed(title=selected["name"], description="買いたいアイテムを選択してください。")
+        for ite in selling_items:
+            if ite["type"] == "weapon":
+                embed.add_field(
+                    name=ite["name"],
+                    value=f"種類：武器 攻撃力：{ite['attack']} 価格：{ite['money']}")
+            elif ite["type"] == "armour":
+                embed.add_field(
+                    name=ite["name"],
+                    value=f"種類：防具 防御力：{ite['block']} 価格：{ite['money']}")
+            elif ite["type"] == "useful":
+                embed.add_field(
+                    name=ite["name"],
+                    value=f"種類：便利アイテム 効果：{ite['effect']} 価格：{ite['money']}")
+        menu = {m["name"]:n for n, m in enumerate(selling_items)}
+        view = utils.EasyView(menu)
+        await inter.response.edit_message(embed=embed, view=view)
+
+        inter = await self.bot.wait_for("interaction", check=inter_check)
+
+        select: discord.ui.Select = view.children[0]  # type: ignore
+        selected = shops[int(select.values[0])]
+        itemid = self.bot.itemdata.index(selected)
+        if udata[6] < selected["money"]:
+            return await inter.response.edit_message(
+                embed=utils.ErrorEmbed("エラー", "お金が足りません。"),
+                view=None
+            )
+        if not str(itemid) in uitem:
+            uitem[str(itemid)] = 1
         else:
-            udata = self.bot.db.user[ctx.author.id]
-            uitem = orjson.loads(self.bot.db.item[ctx.author.id][1])
-            places = [p for p in self.bot.fielddata if p["visit"] < udata[2]]
-            shops = [x for s in places for x in s["shop"]]
-            msg = await ctx.send(
-                embed=discord.Embed(
-                    title="ショップ",
-                    description="行きたいお店に行くことができます。"
-                ),
-                components=[utils.EasyMenu(
-                    "お店を選択", 
-                    "行きたいお店を選択してください。",
-                    **{s["name"]:str(n) for n, s in enumerate(shops)}
-                )]
-            )
-            inter = await msg.wait_for_dropdown(lambda i:i.author == ctx.author)
-            selected = shops[int(inter.select_menu.selected_options[0].value)]
-            selling_items = [self.bot.itemdata[l] for l in selected["items"]]
-            embed = discord.Embed(title=selected["name"], description="買いたいアイテムを選択してください。")
-            for ite in selling_items:
-                if ite["type"] == "weapon":
-                    embed.add_field(
-                        name=ite["name"],
-                        value=f"種類：武器 攻撃力：{ite['attack']} 価格：{ite['money']}")
-                elif ite["type"] == "armour":
-                    embed.add_field(
-                        name=ite["name"],
-                        value=f"種類：防具 防御力：{ite['block']} 価格：{ite['money']}")
-                elif ite["type"] == "useful":
-                    embed.add_field(
-                        name=ite["name"],
-                        value=f"種類：便利アイテム 効果：{ite['effect']} 価格：{ite['money']}")
-            menu = {m["name"]:n for n, m in enumerate(selling_items)}
-            await msg.edit(embed=embed, components=[menu])
-
-            inter = await msg.wait_for_dropdown(lambda i:i.author == ctx.author)
-
-            selected = selling_items[int(inter.select_menu.selected_options[0].value)]
-            itemid = self.bot.itemdata.index(selected)
-            if udata[6] < selected["money"]:
-                return await ctx.edit(
-                    embed=utils.ErrorEmbed("エラー", "お金が足りません。"),
-                    components=[]
-                )
-            if not str(itemid) in uitem:
-                uitem[str(itemid)] = 1
-            else:
-                uitem[str(itemid)] += 1
-            # itemテーブルへ書き込み
-            self.bot.db.item[ctx.author.id]["Data"] = orjson.dumps(uitem)
-            udata[6] -= selected["money"]
-            # moneyを減らしてusersテーブルへ書き込み
-            self.bot.db.user[ctx.author.id]["Money"] = udata[6]
-            await msg.edit(
-                embed=discord.Embed(
-                    title="成功",
-                    description=f"{selected['name']}を購入しました。"
-                ),
-                components=[]
-            )
+            uitem[str(itemid)] += 1
+        # itemテーブルへ書き込み
+        self.bot.db.item[ctx.author.id]["Data"] = orjson.dumps(uitem)
+        udata[6] -= selected["money"]
+        # moneyを減らしてusersテーブルへ書き込み
+        self.bot.db.user[ctx.author.id]["Money"] = udata[6]
+        await inter.response.edit_message(
+            embed=discord.Embed(
+                title="成功",
+                description=f"{selected['name']}を購入しました。"
+            ),
+            view=None
+        )
 
     async def tutorial(self, ctx: commands.Context):
         e = discord.Embed(
